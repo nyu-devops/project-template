427466b826297c139198a81d5aa96679c9d028d6
John Rofrano <johnnyroy@johnrofrano.com>
2022-07-21 17:10:02 -0400

    Fixed bad postgres url

--- service/config.py
+++ service/config.py
@@ -5,7 +5,7 @@ import os
 
 # Get configuration from environment
 DATABASE_URI = os.getenv(
-    "DATABASE_URI", "postgres://postgres:postgres@localhost:5432/postgres"
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
 
 # Configure SQLAlchemy
