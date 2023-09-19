0ae88188175e723c3dfb138ae9b4bb8df5a6e44f
John Rofrano <johnnyroy@johnrofrano.com>
2023-09-19 07:37:40 -0400

    Added more tools

--- .devcontainer/scripts/install-tools.sh
+++ .devcontainer/scripts/install-tools.sh
@@ -5,20 +5,50 @@
 ######################################################################
 
 echo "**********************************************************************"
+echo "Establishing Architecture..."
+echo "**********************************************************************"
+ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')"
+echo "Architecture is:" $ARCH
+
+echo "**********************************************************************"
 echo "Installing K3D Kubernetes..."
 echo "**********************************************************************"
 curl -s "https://raw.githubusercontent.com/rancher/k3d/main/install.sh" | sudo bash
 echo "Creating kc and kns alias for kubectl..."
 echo "alias kc='/usr/local/bin/kubectl'" >> $HOME/.bash_aliases
 echo "alias kns='kubectl config set-context --current --namespace'" >> $HOME/.bash_aliases
+sudo sh -c 'echo "127.0.0.1 cluster-registry" >> /etc/hosts'
 
 echo "**********************************************************************"
-echo "Installing IBM Cloud CLI..."
+echo "Install Kustomize CLI..."
 echo "**********************************************************************"
-curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
-echo "source /usr/local/ibmcloud/autocomplete/bash_autocomplete" >> $HOME/.bashrc
-# Install user mode IBM Cloud plugins
-ibmcloud plugin install container-registry -r 'IBM Cloud'
-ibmcloud plugin install kubernetes-service -r 'IBM Cloud'
-echo "Creating aliases for ibmcloud tools..."
-echo "alias ic='/usr/local/bin/ibmcloud'" >> $HOME/.bash_aliases
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
+sudo mv kustomize /usr/local/bin/kustomize
+echo "Creating ku alias for kustomize..."
+echo "alias ku='/usr/local/bin/kustomize'" >> $HOME/.bash_aliases
+
+echo "**********************************************************************"
+echo "Installing Skaffold..."
+echo "**********************************************************************"
+curl -Lo skaffold "https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-$ARCH"
+sudo install skaffold /usr/local/bin/
+rm skaffold
+
+echo "**********************************************************************"
+echo "Installing K9s..."
+echo "**********************************************************************"
+curl -L -o k9s.tar.gz "https://github.com/derailed/k9s/releases/download/v0.27.3/k9s_Linux_$ARCH.tar.gz"
+tar xvzf k9s.tar.gz
+sudo install -c -m 0755 k9s /usr/local/bin
+rm k9s.tar.gz
+
+echo "**********************************************************************"
+echo "Install Tekton CLI..."
+echo "**********************************************************************"
+curl -LO https://github.com/tektoncd/cli/releases/download/v0.18.0/tkn_0.18.0_Linux_$ARCH.tar.gz
+tar xvzf tkn_0.18.0_Linux_$ARCH.tar.gz -C /usr/local/bin/ tkn
+ln -s /usr/local/bin/tkn /usr/bin/tkn
+
+echo "**********************************************************************"
+echo "TOOLS INSTALLATION COMPLETE !!!"
+echo "**********************************************************************"
