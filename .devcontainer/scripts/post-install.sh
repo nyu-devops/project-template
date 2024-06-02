2aa5e9ed09ac423e7a36a79e196c0a1a5628ef01
John Rofrano <johnnyroy@johnrofrano.com>
2024-06-02 18:53:01 +0000

    Updated tools

--- .devcontainer/scripts/post-install.sh
+++ .devcontainer/scripts/post-install.sh
@@ -3,6 +3,16 @@
 # These must be installed as a user and therefore need to be run
 # after the comtainer has been created.
 #
+echo "**********************************************************************"
+echo "Setting up Docker lab environment..."
+echo "**********************************************************************"
+
 echo Setting up registry.local...
 sudo bash -c "echo '127.0.0.1    cluster-registry' >> /etc/hosts"
+
+echo "Making git stop complaining about unsafe folders"
+git config --global --add safe.directory /app
+
+echo "**********************************************************************"
 echo "Setup complete"
+echo "**********************************************************************"
